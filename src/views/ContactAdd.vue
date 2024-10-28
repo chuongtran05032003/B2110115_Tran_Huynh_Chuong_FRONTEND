fdf8c5400bdc5066590960ea9034e0be58ae4a38
chuongtran <chuongtran05032003@gmail.com>
2024-10-28 23:14:58 +0700

    Tao trang them lien he

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-        <h4 class="flex justify-center font-bold text-[20px] mt-[20px] text-green-700">Thêm liên hệ mới</h4>
+        <h4 class="flex justify-center font-bold text-[20px] mt-[20px] text-green-700">
+            Thêm liên hệ mới
+        </h4>
         <ContactForm @submit:contact="addContact"/>
     </div>
 </template>
-
 <script setup>
 import ContactForm from '@/components/ContactForm.vue';
 import ContactService from '@/services/contact.service';
 import { useRouter} from 'vue-router';
-
 const router = useRouter();
 const addContact = async(data) =>{
     await ContactService.create(data);
